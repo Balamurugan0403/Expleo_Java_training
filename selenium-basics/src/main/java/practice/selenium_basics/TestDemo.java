a766dea9628ac877ac7a063130a68dcd203c43d0
Balamurugan <murugan020325@gmail.com>
2026-04-01 11:27:22 +0530

    first selenium commit

---
+++ selenium-basics/src/main/java/practice/selenium_basics/TestDemo.java
@@ -0,0 +1,27 @@
+package practice.selenium_basics;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			WebDriver driver=new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.get("https://www.flipkart.com/");
+			String urltitle=driver.getTitle();
+			
+			System.out.println("URL TITLE:"+urltitle+"\ntitle length:"+urltitle.length());
+			System.out.println("current url:"+driver.getCurrentUrl());
+			String pagesourcelen=driver.getPageSource();
+			System.out.println("page source length:"+pagesourcelen.length());
+//			driver.getPageSource();
+			
+			driver.quit(); 
+
+	}
+
+}
+
+
